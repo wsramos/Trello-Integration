b07c97f906f9f9a6f577b9c22ef725fdd5a3bae8
William Ramos <contato.williamramos@outlook.com>
2024-05-16 22:30:28 -0300

    Projeto e estrutura criados

---
+++ src/main/java/com/br/lorienmentors/trellointegration/TrellointegrationApplication.java
@@ -0,0 +1,13 @@
+package com.br.lorienmentors.trellointegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrellointegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrellointegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/lorienmentors/trellointegration/TrellointegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.lorienmentors.trellointegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrellointegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
